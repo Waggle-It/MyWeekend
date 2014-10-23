8cfad72244a05cb5bd8da399c291d7cedef4c0f4
Angela Zhu <angelazhu96@gmail.com>
2014-10-22 19:44:04 -0500

    event 10/22/14

---
+++ app/src/main/java/com/example/jozsef/myweekend/Event.java
@@ -0,0 +1,52 @@
+package com.example.jozsef.myweekend;
+public class Event {
+    private String title, location, date, description ;
+    private boolean hasFood;
+    private double price;
+    public Event(String title, String location, String date, String description, boolean hasFood, double price){
+        this.title = title;
+        this.location = location;
+        this.date = date;
+        this.description = description;
+        this.hasFood = hasFood;
+        this.price = price;
+    }
+    public Event(){
+    }
+    public String getTitle(){
+        return title;
+    }
+    public String getLocation(){
+        return location;
+    }
+    public String getDate(){
+        return date;
+    }
+    public String getDescription(){
+        return description;
+    }
+    public boolean hasFood(){
+        return hasFood;
+    }
+    public double getPrice(){
+        return price;
+    }
+    public void setTitle(String title){
+        this.title = title;
+    }
+    public void setLocation(String location){
+        this.location = location;
+    }
+    public void setDate(String date){
+        this.date = date;
+    }
+    public void setDescription(String description){
+        this.description = description;
+    }
+    public void setHasFood(boolean hasFood){
+        this.hasFood = hasFood;
+    }
+    public void setPrice(double price){
+        this.price = price;
+    }
+}
