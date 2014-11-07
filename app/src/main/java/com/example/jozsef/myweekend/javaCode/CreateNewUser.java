8e9f9a7efb02495268d1e7bfd30c2093657681c9
jozsefmorrissey <jtmorri2@illinois.edu>
2014-11-06 22:34:49 -0600

    split the java and adroid code for new user

---
+++ app/src/main/java/com/example/jozsef/myweekend/javaCode/CreateNewUser.java
@@ -0,0 +1,97 @@
+package com.example.jozsef.myweekend.javaCode;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.jozsef.myweekend.R;
+import com.example.jozsef.myweekend.User;
+import com.example.jozsef.myweekend.UserList;
+import com.example.jozsef.myweekend.android.Login;
+import com.example.jozsef.myweekend.android.NewUser;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by Jozsef on 11/5/2014.
+ */
+public class CreateNewUser extends NewUser{
+
+    public boolean create(String email, String password, String confirmPassword,int zip){
+
+        int[] preferancesLike = new int[21];
+
+        if(verifyEmail(email) && verifyPassword(password, confirmPassword) && verifyZip(zip) && uniqueUser(email) && verifyEmail(email)) {
+            for (int i = 0; i < 21; i++)
+                preferancesLike[i] = 0;
+
+
+            User newUser = new User(email, password, zip, preferancesLike);
+            UserList.addUser(newUser);
+
+            return true;
+        }
+        return false;
+    }
+
+    private boolean verifyPassword(String password, String confirmPassword){
+        if (password.length()>7) {
+            if(password.equals(confirmPassword))
+                return true;
+            else
+                super.passwordMatchError();
+        }
+        else
+            super.passWordParameterError();
+
+        return false;
+    }
+
+
+    private boolean verifyZip(int Zip){
+        if(Integer.toString(Zip).length()==5) {
+            return true;
+        }
+        else
+            super.validZipError();
+
+        return false;
+    }
+
+    private boolean uniqueUser(String email){
+
+        boolean unique = true;
+
+        for(int i=0; i<UserList.getUserListLength(); i++) {
+            if (UserList.getUserEmail(i).equals(email))
+                unique = false;
+        }
+        if(!unique)
+            super.acctAlreadyRegisteredError();
+
+        return unique;
+    }
+
+    //Code borrowed from http://examples.javacodegeeks.com/
+    private boolean verifyEmail(String email){
+
+        Pattern pattern;
+        Matcher matcher;
+
+        String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+        pattern = Pattern.compile(EMAIL_PATTERN);
+
+        matcher = pattern.matcher(email);
+
+        if(!matcher.matches())
+            super.invalidEmailError();
+
+        return matcher.matches();
+
+
+    }
+}
