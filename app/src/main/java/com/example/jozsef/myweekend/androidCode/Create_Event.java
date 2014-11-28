03e165095e66b0b52b39b0fa4baf9b0f15a5552c
jozsefmorrissey <jtmorri2@illinois.edu>
2014-11-28 16:13:37 -0600

    Implemented controls on user inputs in create event menu.

--- app/src/main/java/com/example/jozsef/myweekend/androidCode/Create_Event.java
+++ app/src/main/java/com/example/jozsef/myweekend/androidCode/Create_Event.java
@@ -1,6 +1,7 @@
 package com.example.jozsef.myweekend.androidCode;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,12 +9,18 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.jozsef.myweekend.R;
+import com.example.jozsef.myweekend.javaCode.CreateNewUser;
 import com.example.jozsef.myweekend.javaCode.Objects.Event;
 import com.example.jozsef.myweekend.javaCode.Objects.EventList;
 import com.example.jozsef.myweekend.javaCode.Objects.Quality;
 import com.example.jozsef.myweekend.javaCode.Objects.UserList;
+import com.example.jozsef.myweekend.javaCode.Objects.VerifyCreateEvent;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 
 /**
@@ -39,12 +46,156 @@ public class Create_Event extends Activity {
 
             @Override
             public void onClick(View v) {
+                VerifyCreateEvent.setDisplay(getApplicationContext());
+                if(verify()) {
+                    create();
+                    startActivity(new Intent(Create_Event.this, SortMenu.class));
+                }
+                else
+                    Toast.makeText(Create_Event.this, "Please fill in indicated Fields (*)", Toast.LENGTH_LONG).show();
 
-                create();
+            }
+        });
+    }
+    //tests user inputs and activates astrix icons beside required inputs.
+    public boolean verify(){
+        boolean returnVal=true;
+        VerifyCreateEvent sugar = new VerifyCreateEvent();
+        if(!sugar.verifyPCEmail(getPCEmail())) {
+            TextView astrix = (TextView)findViewById(R.id.Required9);
+            astrix.setVisibility(View.VISIBLE);
+            TextView required = (TextView)findViewById(R.id.textViewRequired);
+            required.setVisibility(View.VISIBLE);
+            returnVal = false;
+        }
+            else{
+                TextView astrix = (TextView)findViewById(R.id.Required9);
+                astrix.setVisibility(View.INVISIBLE);
             }
 
+        if(!sugar.verifyEventDate(getEventDateString())) {
+            TextView astrix = (TextView)findViewById(R.id.Required2);
+            astrix.setVisibility(View.VISIBLE);
+            TextView required = (TextView)findViewById(R.id.textViewRequired);
+            required.setVisibility(View.VISIBLE);
+            returnVal = false;
+        }
+            else{
+                TextView astrix = (TextView)findViewById(R.id.Required2);
+                astrix.setVisibility(View.INVISIBLE);
+            }
+        if(!sugar.verifyEventTime(getTime())){
+            TextView astrix = (TextView)findViewById(R.id.Required3);
+            astrix.setVisibility(View.VISIBLE);
+            TextView required = (TextView)findViewById(R.id.textViewRequired);
+            required.setVisibility(View.VISIBLE);
+            returnVal = false;
+        }
+            else{
+                TextView astrix = (TextView)findViewById(R.id.Required3);
+                astrix.setVisibility(View.INVISIBLE);
+            }
 
-        });
+        if(!sugar.verifyEventDisc(getEventDescription())) {
+            TextView astrix = (TextView)findViewById(R.id.Required8);
+            astrix.setVisibility(View.VISIBLE);
+            TextView required = (TextView)findViewById(R.id.textViewRequired);
+            required.setVisibility(View.VISIBLE);
+            returnVal = false;
+        }
+            else{
+                TextView astrix = (TextView)findViewById(R.id.Required8);
+                astrix.setVisibility(View.INVISIBLE);
+            }
+
+        if(!sugar.verifyEventTitle(getEventTitle())) {
+            TextView astrix = (TextView)findViewById(R.id.Required1);
+            astrix.setVisibility(View.VISIBLE);
+            TextView required = (TextView)findViewById(R.id.textViewRequired);
+            required.setVisibility(View.VISIBLE);
+            returnVal = false;
+        }
+            else{
+                TextView astrix = (TextView)findViewById(R.id.Required1);
+                astrix.setVisibility(View.INVISIBLE);
+            }
+
+        if(!sugar.verifyFoodCosts(getEventFoodCosts(), onOff().isFood())) {
+            TextView astrix = (TextView)findViewById(R.id.Required10);
+            astrix.setVisibility(View.VISIBLE);
+            TextView required = (TextView)findViewById(R.id.textViewRequired);
+            required.setVisibility(View.VISIBLE);
+            returnVal = false;
+        }
+            else{
+                TextView astrix = (TextView)findViewById(R.id.Required10);
+                astrix.setVisibility(View.INVISIBLE);
+            }
+
+        if(!sugar.verifyEventCity(getEventCity())) {
+            TextView astrix = (TextView)findViewById(R.id.Required5);
+            astrix.setVisibility(View.VISIBLE);
+            TextView required = (TextView)findViewById(R.id.textViewRequired);
+            required.setVisibility(View.VISIBLE);
+            returnVal = false;
+        }
+            else{
+                TextView astrix = (TextView)findViewById(R.id.Required5);
+                astrix.setVisibility(View.INVISIBLE);
+            }
+
+        if(!sugar.verifyEventAddress(getEventAddress())) {
+            TextView astrix = (TextView)findViewById(R.id.Required4);
+            astrix.setVisibility(View.VISIBLE);
+            TextView required = (TextView)findViewById(R.id.textViewRequired);
+            required.setVisibility(View.VISIBLE);
+            returnVal = false;
+        }
+            else{
+                TextView astrix = (TextView)findViewById(R.id.Required4);
+                astrix.setVisibility(View.INVISIBLE);
+            }
+
+        if(!sugar.verifyZip(getEventZipCode())) {
+            TextView astrix = (TextView)findViewById(R.id.Required7);
+            astrix.setVisibility(View.VISIBLE);
+            TextView required = (TextView)findViewById(R.id.textViewRequired);
+            required.setVisibility(View.VISIBLE);
+            returnVal = false;
+        }
+            else{
+                TextView astrix = (TextView)findViewById(R.id.Required7);
+                astrix.setVisibility(View.INVISIBLE);
+            }
+
+        if(!sugar.verifyEventState(getEventState())) {
+            TextView astrix = (TextView)findViewById(R.id.Required6);
+            astrix.setVisibility(View.VISIBLE);
+            TextView required = (TextView)findViewById(R.id.textViewRequired);
+            required.setVisibility(View.VISIBLE);
+            returnVal = false;
+        }
+            else{
+                TextView astrix = (TextView)findViewById(R.id.Required6);
+                astrix.setVisibility(View.INVISIBLE);
+            }
+
+        if(!sugar.verifyTicketCosts(getEventTicketCosts())) {
+            TextView astrix = (TextView)findViewById(R.id.Required11);
+            astrix.setVisibility(View.VISIBLE);
+            TextView required = (TextView)findViewById(R.id.textViewRequired);
+            required.setVisibility(View.VISIBLE);
+            returnVal = false;
+        }
+        else{
+            TextView astrix = (TextView)findViewById(R.id.Required11);
+            astrix.setVisibility(View.INVISIBLE);
+        }
+
+
+
+        return returnVal;
+
     }
     //Method is activated when the user hits the submit button all fields necessary to create a new event are retrieved.
     private void create(){
@@ -57,9 +208,9 @@ public class Create_Event extends Activity {
         categories = onOff();
 
         title = getEventTitle();
-        location = getEventLocation();
+        location = getEventAddress() + " " + getEventCity() + ", " + getEventState() + " " + getEventZipCode();
         description = getEventDescription();
-        date = getEventDate();
+        date = getEventDateLong();
         pcName = getEventPCName();
         pcEmail = getPCEmail();
         submittedBy = getSubmittedBy();
@@ -74,32 +225,48 @@ public class Create_Event extends Activity {
         EventList.getEventList().add(nEw);
 
     }
+    public String getTime(){
+        TextView time = (TextView)findViewById(R.id.Time);
+        return time.getText().toString();
+    }
     public String getURL(){
         TextView url = (TextView)findViewById(R.id.eventURL);
         return url.getText().toString();
     }
+
     public String getSubmittedBy(){
-        String user = UserList.getCurrentUser().getEmail();
-        int i = user.indexOf("@");
-        user = user.substring(0, i);
-        return user;
+        return UserList.getCurrentUser().getEmail();
     }
 
     //Returns Admission costs
     public double getEventTicketCosts(){
         EditText ticketCosts = (EditText)findViewById(R.id.ticketCosts);
-        return Double.parseDouble(ticketCosts.getText().toString());
+        try {
+            return Double.parseDouble(ticketCosts.getText().toString());
+        }
+        catch(Exception e){}
+
+            return -1.0;
     }
 
     //Returns Food Costs
     public double getEventFoodCosts(){
         EditText foodCost = (EditText)findViewById(R.id.foodCost);
-        return Double.parseDouble(foodCost.getText().toString());
+        try {
+            double x = Double.parseDouble(foodCost.getText().toString());
+            if(x>=0 && x<=200)
+                return x;
+        }
+        catch(Exception e){}
+
+        return -1.0;
     }
 
     //Returns point of contact email
     public String getPCEmail() {
         EditText email = (EditText)findViewById(R.id.pcmail);
+        if(email == null)
+            return "";
         return email.getText().toString();
     }
 
@@ -109,10 +276,24 @@ public class Create_Event extends Activity {
         return name.getText().toString();
     }
 
+    public String getEventDateString(){
+        return ((EditText)findViewById(R.id.Date)).getText().toString();
+    }
     //Returns Date and time variable
-    public long getEventDate() {
+    public long getEventDateLong() {
+        Long uniTime = 0L;
+        DateFormat dfm = new SimpleDateFormat("yyyyMMddHHmm");
+        String date = getEventDateString();
+        String time = getTime();
+        String temp = "20" + date.substring(6, date.length()) + date.substring(0, 2) + date.substring(3, 5) + time.substring(0, time.indexOf(":")) + time.substring(time.indexOf(":")+1, time.length());
+        Toast.makeText(Create_Event.this, temp, Toast.LENGTH_LONG).show();
 
-        return 0;
+        try{
+            uniTime = dfm.parse(temp).getTime();
+        }catch (Exception e){}
+
+
+        return uniTime;
     }
 
     //Returns Event description
@@ -121,44 +302,67 @@ public class Create_Event extends Activity {
         return desc.getText().toString();
     }
 
-    //Returns Event Location
-    public String getEventLocation() {
-        EditText location = (EditText)findViewById(R.id.Location);
+    //Returns Event Address
+    public String getEventAddress() {
+        EditText location = (EditText)findViewById(R.id.Address);
         return location.getText().toString();
     }
 
+    //Returns Event City
+    public String getEventCity() {
+        EditText location = (EditText) findViewById(R.id.City);
+        return location.getText().toString();
+    }
+
+        //Returns Event State
+    public String getEventState() {
+        EditText location = (EditText) findViewById(R.id.State);
+        return location.getText().toString();
+    }
+
+    //Returns Event Zip Code
+    public int getEventZipCode() {
+            EditText zip = (EditText)findViewById(R.id.ZipCode);
+
+            try {
+                return Integer.parseInt(zip.getText().toString());
+            }
+            catch(Exception e){}
+
+            return 0;
+        }
+
     //Returns event title
-    private String getEventTitle(){
+    public String getEventTitle(){
         EditText title = (EditText)findViewById(R.id.EventTitle);
         return title.getText().toString();
     }
 
-    private int getImage (){return R.drawable.ic_launcher;}
+    public int getImage (){return R.drawable.ic_launcher;}
 
     //Returns an array of booleans that help characterize the event.
-    private Quality onOff(){
+    public Quality onOff() {
 
-        CheckBox autoBoatAir = (CheckBox)findViewById(R.id.AutoBoatAir);
-        CheckBox businessProfessional = (CheckBox)findViewById(R.id.BusinessProfessional);
-        CheckBox charityCauses = (CheckBox)findViewById(R.id.CharityCauses);
-        CheckBox familyEducation = (CheckBox)findViewById(R.id.FamilyEducation);
-        CheckBox fashionBeauty = (CheckBox)findViewById(R.id.FashionBeauty);
-        CheckBox filmMediaEntertainment = (CheckBox)findViewById(R.id.FilmMediaEntertainment);
-        CheckBox food = (CheckBox)findViewById(R.id.food);
-        CheckBox foodDrink = (CheckBox)findViewById(R.id.FoodDrink);
-        CheckBox governmentPolitics = (CheckBox)findViewById(R.id.GovernmentPolitics);
-        CheckBox healthWellness = (CheckBox)findViewById(R.id.HealthWellness);
-        CheckBox hobbiesSpecialInterest = (CheckBox)findViewById(R.id.HobbiesSpecialInterest);
-        CheckBox homeLifestyle = (CheckBox)findViewById(R.id.HomeLifestyle);
-        CheckBox music = (CheckBox)findViewById(R.id.Music);
-        CheckBox other = (CheckBox)findViewById(R.id.Other);
-        CheckBox performingVisualArts = (CheckBox)findViewById(R.id.PerformingVisualArts);
-        CheckBox religionSpirituality = (CheckBox)findViewById(R.id.ReligionSpirituality);
-        CheckBox scienceTechnology = (CheckBox)findViewById(R.id.ScienceTechnology);
-        CheckBox seasonalHoliday = (CheckBox)findViewById(R.id.SeasonalHoliday);
-        CheckBox sportsFitness = (CheckBox)findViewById(R.id.SportsFitness);
-        CheckBox ticket = (CheckBox)findViewById(R.id.ticket);
-        CheckBox travelOutdoor = (CheckBox)findViewById(R.id.TravelOutdoor);
+        CheckBox autoBoatAir = (CheckBox) findViewById(R.id.AutoBoatAir);
+        CheckBox businessProfessional = (CheckBox) findViewById(R.id.BusinessProfessional);
+        CheckBox charityCauses = (CheckBox) findViewById(R.id.CharityCauses);
+        CheckBox familyEducation = (CheckBox) findViewById(R.id.FamilyEducation);
+        CheckBox fashionBeauty = (CheckBox) findViewById(R.id.FashionBeauty);
+        CheckBox filmMediaEntertainment = (CheckBox) findViewById(R.id.FilmMediaEntertainment);
+        CheckBox food = (CheckBox) findViewById(R.id.food);
+        CheckBox foodDrink = (CheckBox) findViewById(R.id.FoodDrink);
+        CheckBox governmentPolitics = (CheckBox) findViewById(R.id.GovernmentPolitics);
+        CheckBox healthWellness = (CheckBox) findViewById(R.id.HealthWellness);
+        CheckBox hobbiesSpecialInterest = (CheckBox) findViewById(R.id.HobbiesSpecialInterest);
+        CheckBox homeLifestyle = (CheckBox) findViewById(R.id.HomeLifestyle);
+        CheckBox music = (CheckBox) findViewById(R.id.Music);
+        CheckBox other = (CheckBox) findViewById(R.id.Other);
+        CheckBox performingVisualArts = (CheckBox) findViewById(R.id.PerformingVisualArts);
+        CheckBox religionSpirituality = (CheckBox) findViewById(R.id.ReligionSpirituality);
+        CheckBox scienceTechnology = (CheckBox) findViewById(R.id.ScienceTechnology);
+        CheckBox seasonalHoliday = (CheckBox) findViewById(R.id.SeasonalHoliday);
+        CheckBox sportsFitness = (CheckBox) findViewById(R.id.SportsFitness);
+        CheckBox travelOutdoor = (CheckBox) findViewById(R.id.TravelOutdoor);
 
         Quality temp = new Quality();
 
@@ -173,6 +377,7 @@ public class Create_Event extends Activity {
         temp.setHealth(healthWellness.isChecked());
         temp.setHobbies(hobbiesSpecialInterest.isChecked());
         temp.setLifestyle(homeLifestyle.isChecked());
+        temp.setFood(food.isChecked());
         temp.setMusic(music.isChecked());
         temp.setOther(other.isChecked());
         temp.setVisual_arts(performingVisualArts.isChecked());
@@ -184,6 +389,4 @@ public class Create_Event extends Activity {
 
         return temp;
     }
-
-
 }
