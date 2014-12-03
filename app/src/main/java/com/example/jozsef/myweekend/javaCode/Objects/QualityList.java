587e79acab0fae029db0dacabb354bd65e39b5d4
jozsefmorrissey <jtmorri2@illinois.edu>
2014-12-02 22:48:20 -0600

    sort menu is nearly functioning

--- app/src/main/java/com/example/jozsef/myweekend/javaCode/Objects/QualityList.java
+++ app/src/main/java/com/example/jozsef/myweekend/javaCode/Objects/QualityList.java
@@ -1,6 +1,14 @@
 package com.example.jozsef.myweekend.javaCode.Objects;
 
+import android.content.Context;
+import android.widget.Toast;
+
+import com.example.jozsef.myweekend.androidCode.SortMenu;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 /**
@@ -30,152 +38,76 @@ public class QualityList {
         return vehicleList;
     }
 
-    public static void setVehicleList(List<Event> vehicleList) {
-        QualityList.vehicleList = vehicleList;
-    }
-
     public static List<Event> getCauseList() {
         return causeList;
-    }
-
-    public static void setCauseList(List<Event> causeList) {
-        QualityList.causeList = causeList;
     }
 
     public static List<Event> getFashionList() {
         return fashionList;
     }
 
-    public static void setFashionList(List<Event> fashionList) {
-        QualityList.fashionList = fashionList;
-    }
-
     public static List<Event> getFoodList() {
         return foodList;
-    }
-
-    public static void setFoodList(List<Event> foodList) {
-        QualityList.foodList = foodList;
     }
 
     public static List<Event> getHealthList() {
         return healthList;
     }
 
-    public static void setHealthList(List<Event> healthList) {
-        QualityList.healthList = healthList;
-    }
-
     public static List<Event> getMusicList() {
         return musicList;
-    }
-
-    public static void setMusicList(List<Event> musicList) {
-        QualityList.musicList = musicList;
     }
 
     public static List<Event> getLifestyleList() {
         return lifestyleList;
     }
 
-    public static void setLifestyleList(List<Event> lifestyleList) {
-        QualityList.lifestyleList = lifestyleList;
-    }
-
     public static List<Event> getTechList() {
         return techList;
-    }
-
-    public static void setTechList(List<Event> techList) {
-        QualityList.techList = techList;
     }
 
     public static List<Event> getHollidayList() {
         return hollidayList;
     }
 
-    public static void setHollidayList(List<Event> hollidayList) {
-        QualityList.hollidayList = hollidayList;
-    }
-
     public static List<Event> getOutdoorList() {
         return outdoorList;
-    }
-
-    public static void setOutdoorList(List<Event> outdoorList) {
-        QualityList.outdoorList = outdoorList;
     }
 
     public static List<Event> getBuisnessList() {
         return buisnessList;
     }
 
-    public static void setBuisnessList(List<Event> buisnessList) {
-        QualityList.buisnessList = buisnessList;
-    }
-
     public static List<Event> getEducationList() {
         return educationList;
-    }
-
-    public static void setEducationList(List<Event> educationList) {
-        QualityList.educationList = educationList;
     }
 
     public static List<Event> getEntertainmentList() {
         return entertainmentList;
     }
 
-    public static void setEntertainmentList(List<Event> entertainmentList) {
-        QualityList.entertainmentList = entertainmentList;
-    }
-
     public static List<Event> getPoliticsList() {
         return politicsList;
-    }
-
-    public static void setPoliticsList(List<Event> politicsList) {
-        QualityList.politicsList = politicsList;
     }
 
     public static List<Event> getHobbiesList() {
         return hobbiesList;
     }
 
-    public static void setHobbiesList(List<Event> hobbiesList) {
-        QualityList.hobbiesList = hobbiesList;
-    }
-
     public static List<Event> getArtsList() {
         return artsList;
-    }
-
-    public static void setArtsList(List<Event> artsList) {
-        QualityList.artsList = artsList;
     }
 
     public static List<Event> getReligiousList() {
         return religiousList;
     }
 
-    public static void setReligiousList(List<Event> religiousList) {
-        QualityList.religiousList = religiousList;
-    }
-
     public static List<Event> getSportsList() {
         return sportsList;
     }
 
-    public static void setSportsList(List<Event> sportsList) {
-        QualityList.sportsList = sportsList;
-    }
-
     public static List<Event> getOtherList() {
         return otherList;
-    }
-
-    public static void setOtherList(List<Event> otherList) {
-        QualityList.otherList = otherList;
     }
 
     private static List<Event> otherList= new ArrayList<Event>();
@@ -184,5 +116,79 @@ public class QualityList {
         qualityList = qualityList;
     }
     public QualityList() {}
+
+    public static Context context;
+
+    public static List<Event> getEventList(String cat){
+
+        if(cat.equals("Search"))
+
+        if(cat.equals("Step Out of Your Box"))
+
+        if(cat.equals("Recommended"))
+
+        if(cat.equals("Popular"));
+
+        if(cat.equals("Today")){
+            List<Event> temp = new ArrayList<Event>();
+            for(int i = 0; i<EventList.getEventList().size(); i++) {
+                long test = EventList.getEventList().get(i).getDate();
+                DateFormat dfm = new SimpleDateFormat("yyyyMMdd");
+                if (dfm.format(test).equals(dfm.format(Calendar.getInstance().getTime())))
+                    temp.add(EventList.getEventList().get(i));
+            }
+            return temp;
+        }
+
+        //Need to add the ability to add to my weekend.
+        if(cat.equals("Your Weekend"))
+            return UserList.getCurrentUser().getMyWeekend();
+        if(cat.equals("All Events"))
+            return EventList.getEventList();
+        if(cat.equals("Auto, Boat & Air"))
+            return getVehicleList();
+        if(cat.equals("Business & Professional"))
+            return getBuisnessList();
+        if(cat.equals("Charity & Causes"))
+            return getCauseList();
+        if(cat.equals("Family & Education"))
+            return getEducationList();
+        if(cat.equals("Fashion & Beauty"))
+            return getFashionList();
+        if(cat.equals("Film, Media & Entertainment"))
+            return getEntertainmentList();
+        if(cat.equals("Food Served?"));
+
+        if(cat.equals("Food Drink"))
+            return getFoodList();
+        if(cat.equals("Government & Politics"))
+            return getPoliticsList();
+        if(cat.equals("Health & Wellness"))
+            return getHealthList();
+        if(cat.equals("Hobbies & Special Interest"))
+            return getHobbiesList();
+        if(cat.equals("Home & Lifestyle"))
+            return getLifestyleList();
+        if(cat.equals("Music"))
+            return getMusicList();
+        if(cat.equals("Other"))
+            return getOtherList();
+        if(cat.equals("Performing & Visual Arts"))
+            return getArtsList();
+        if(cat.equals("Religion & Spirituality"))
+            return getReligiousList();
+        if(cat.equals("Science & Technology"))
+            return getTechList();
+        if(cat.equals("Seasonal & Holiday"))
+            return getHollidayList();
+        if(cat.equals("Sports and Fitness"))
+            return getSportsList();
+        if(cat.equals("Admission Costs?"));
+
+        if(cat.equals("Travel & Outdoor"))
+            return getOutdoorList();
+
+        return EventList.getEventList();
+    }
 
 }
