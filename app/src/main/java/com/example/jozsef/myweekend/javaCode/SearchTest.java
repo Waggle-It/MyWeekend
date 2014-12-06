14f4b802717ee36457570b64c8b5c4e5cbecd08c
jozsefmorrissey <jtmorri2@illinois.edu>
2014-12-06 14:52:27 -0600

    user preferences & customized list opperational

---
+++ app/src/main/java/com/example/jozsef/myweekend/javaCode/SearchTest.java
@@ -0,0 +1,17 @@
+package com.example.jozsef.myweekend.javaCode;
+
+/**
+ * Created by Jozsef on 12/6/2014.
+ */
+public class SearchTest {
+    public int titleContains(String[] keyWords){
+        return 0;
+    }
+    public int descriptionContains(String[] keyWords){
+        return 0;
+    }
+    public boolean dateWithinConstaraints(long start, long end){
+        return true;
+    }
+
+}
